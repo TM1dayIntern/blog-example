55ef666b88694cdbd35b3304ce48324f61d73b01
tm-umino <t.umino@tcmobile.jp>
2018-01-31 11:45:46 +0900

    guide更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password validations: true
   validates :name, presence:true
   validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 end
